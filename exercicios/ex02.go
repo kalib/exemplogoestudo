bd869b7569aa101bfc668b7f50eb75901781b645
Marcelo Cavalcante Rocha <marcelo.rocha@flipp.com>
2021-03-03 20:36:09 -0500

    Exercicios 01 e 02

---
+++ exercicios/ex02.go
@@ -0,0 +1,22 @@
+// Altere o código a seguir e inclua o seguinte:
+// - Crie uma nova variável chamada nota1
+// - Crie uma variável chamada nota2
+// - Crie uma variável chamada nota3
+// - Crie uma variável chamada media:
+// -- A variável media deverá receber o valor da media entre nota1, nota2 e nota3
+// - Imprima o resultado da media
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	soma := nota1 + nota2 + nota3
+ggsergse hseybsy
+bserynserysterynmsreconstneysryeswybrsconst
+bwstywrtnywtrsconst
+bweyrnbweywytewr
+	fmt.Println("O resultado de soma é:", soma)
+}
